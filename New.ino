abcb75a084e126cefd2ad15fd50048e52c0ed127
Venkatesh <kamath797@gmail.com>
2018-10-31 20:38:52 +0530

    Completed the ino code

--- New.ino
+++ New.ino
@@ -2,12 +2,13 @@
 const int motorPinA = 10;// PIN 7 OF L293D 
 const int motorPinB = 9;//PIN 2 OF L293D
 
-
 // PROXIMITY CONNECTION
 const int led = 13; //led connection 
 const int obstaclePin = 7;// Output of proximity sensor
 const int hasObstacle = HIGH;//inital condition of led
 
+//Data comming from python
+int data;
 
 void setup(){
     pinMode(led,OUTPUT);
@@ -20,5 +21,49 @@ void setup(){
 
 void loop(){
     hasObstacle = digitalRead(obstaclePin);
-    Ser
+    Serial.println(hasObstacle);
+    while(Serial.available()){
+        data=Serial.read();
+        if (data == 1)
+            {
+              //Locks the door by clockwise direction of motor
+                digitalWrite(motorPin3, HIGH);
+                digitalWrite(motorPin4, LOW);
+                delay(2000); 
+                //This code will turn Motor B counter-clockwise for 2 sec.
+                digitalWrite(motorPin3, LOW);
+                digitalWrite(motorPin4, HIGH);
+                delay(500);    
+                digitalWrite(motorPin3, LOW);
+                digitalWrite(motorPin4, LOW);  
+            }
+        else
+            {
+                // Unlocks the door by rotating anticlockwise
+                digitalWrite(motorPin3, HIGH);
+                digitalWrite(motorPin4, LOW);
+                delay(500); 
+                digitalWrite(motorPin3, LOW);
+                digitalWrite(motorPin4, HIGH);
+                delay(2000);    
+                digitalWrite(motorPin3, LOW);
+                digitalWrite(motorPin4, LOW);  
+        }
+    }
+
+
+
+
+    if (hasObstacle == LOW) //LOW means something is ahead, so illuminates the 13th Port connected LED
+    {
+    // Serial.println("Stop something is ahead!!");
+        digitalWrite(LED, HIGH);//Illuminates the 13th Port LED
+    }
+    else
+    {
+    //Serial.println("Path is clear");
+        digitalWrite(LED, LOW);
+    }
+    delay(1500);
+}
 }
